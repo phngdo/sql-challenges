0007ea84d3ce906375b98bd7af833d8a7770076d
Phuong Le Do <joni.phngdo@gmail.com>
2025-01-08 21:49:04 +0700

    Update Exercise

--- Pineapple Data/Round 01 - Basic/Day 02 - 05.12.2024/Exercise.sql
+++ Pineapple Data/Round 01 - Basic/Day 02 - 05.12.2024/Exercise.sql
@@ -1,11 +1,11 @@
 -- 01. Tìm những sinh viên có GPA thấp hơn 2.0 và học ngành "Công nghệ thông tin”
 SELECT Student_ID,
-	   Full_Name,
-	   Age,
-	   Major,
-	   GPA,
-	   Enrollment_Date,
-	   Graduation_Year
+	     Full_Name,
+	     Age,
+	     Major,
+	     GPA,
+	     Enrollment_Date,
+	     Graduation_Year
 FROM dbo.Students
 WHERE GPA < 2.0
   AND Major = 'Computer Sci';
