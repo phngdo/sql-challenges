d5b63be245c244db9f9659ab7ee590796e6505ba
Phuong Le Do <joni.phngdo@gmail.com>
2025-01-09 21:42:18 +0700

    Update SampleDB

--- Pineapple Data/Round 01 - Basic/Day 03 - 06.12.2024/SampleDB.sql
+++ Pineapple Data/Round 01 - Basic/Day 03 - 06.12.2024/SampleDB.sql
@@ -4,3 +4,14 @@ GO
 
 USE SampleDB
 GO
+
+-- Create table
+CREATE TABLE Employees
+(
+  employee_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  first_name NVARCHAR(100) NOT NULL,
+  last_name NVARCHAR(100) NOT NULL,
+  department NVARCHAR(50) NOT NULL,
+  salary DECIMAL(15,2),
+  hire_date DATE
+);
